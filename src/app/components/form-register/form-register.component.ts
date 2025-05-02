6364f520fbf6fb4c64f55fc4fa7ae66dea36cdbc
Jodair Nunes <jodairnunes@hotmail.com>
2025-05-02 19:46:31 -0300

    commit 01 - create pages and components default

---
+++ src/app/components/form-register/form-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-register',
+  imports: [],
+  templateUrl: './form-register.component.html',
+  styleUrl: './form-register.component.scss'
+})
+export class FormRegisterComponent {
+
+}
