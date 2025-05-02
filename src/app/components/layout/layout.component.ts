ef735241c15db4c410c1917eefaaf4a27d97fb22
Jodair Nunes <jodairnunes@hotmail.com>
2025-05-02 20:09:04 -0300

    commit 03 - create html, css and metods layout component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormRegisterComponent } from '../form-register/form-register.component';
+import { FormLoginComponent } from '../form-login/form-login.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [FormRegisterComponent, FormLoginComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
 
+  showFormLogin: boolean = true;
+  showFormRegister: boolean = false;
+
+  titleRegister: string = '';
+
+  ngOnInit(): void {
+      this.setTitleBtn()
+  }
+
+  setTitleBtn() {
+    if(this.showFormLogin) {
+      this.titleRegister = 'Register'
+    }else {
+      this.titleRegister = 'Login';
+    }
+  }
+
+  goToRegisterLogin() {
+    this.showFormLogin = !this.showFormLogin;
+    this.showFormRegister = !this.showFormRegister;
+    this.setTitleBtn();
+  }
 }
