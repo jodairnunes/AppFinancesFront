d997ed70589b6571dc2f6d0e071dcb0173961a29
Jodair Nunes <jodairnunes@hotmail.com>
2025-05-02 20:40:20 -0300

    commit 07 - create Guard and import canActivate in the routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { authGuard } from './Guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LayoutComponent},
-    {path: 'dashboard', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
     {path: '**', redirectTo: 'login'}
 ];
