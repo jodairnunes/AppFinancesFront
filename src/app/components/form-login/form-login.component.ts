6a8831142e03864a2a941ac97f5f462f93b245ba
Jodair Nunes <jodairnunes@hotmail.com>
2025-05-02 20:30:38 -0300

    commit 04 - imports components login and register to layout, create html, css and metods form-login and form-register

--- src/app/components/form-login/form-login.component.ts
+++ src/app/components/form-login/form-login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-login.component.html',
   styleUrl: './form-login.component.scss'
 })
 export class FormLoginComponent {
+
+  formLogin!: FormGroup;
+
+
+  constructor(private router: Router) {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
 
 }
--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-register.component.html',
   styleUrl: './form-register.component.scss'
 })
 export class FormRegisterComponent {
+  formregister!: FormGroup;
 
+  constructor() {
+    this.formregister = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
